d7f3384d2801102f1f6c89689b3f36e5cce984bb
Giovanni <111045802+Giovi06@users.noreply.github.com>
2023-10-18 08:34:35 +0200

    Add files via upload

---
+++ PlayerCombat.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Transform attackPoint;
+
+    public Animator animator;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+
+    public float attackRange = 2f;
+    float nextAttackTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                Attack();
+            }
+        }
+        
+
+
+    }
+    void Attack() {
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies) {
+            enemy.GetComponent<Player>().TakeDamage(attackDamage);
+            
+        }
+    }
+    void OnDrawGizmosSelected() {
+        if (attackPoint == null) { return; }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
